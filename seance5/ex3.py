b864d2ac453c80114134395747f9519de145bddd
luciedtx <lucie.doutriaux@gmail.com>
2023-11-29 19:33:52 +0100

    updt

--- seance5/ex3.py
+++ seance5/ex3.py
@@ -1,5 +1,14 @@
 class Vehicule():
-    def __init__(self, type, nbplace):
-        self.type = type
-        self.nbplace= nbplace
-        
+    def init(self, _type, _nbplace):
+        self._type = _type
+        self._nbplace= _nbplace
+    def get_type(self):
+        return self._type
+    def set_type(self, _type):
+        self._type= _type
+        return self._type
+    def get_nbplace(self):
+        return self._nbplace
+    def set_nbplace(self, _nbplace):
+        self._nbplace= _nbplace
+        return self._nbplace
---
+++ seance5/ex3id.py
@@ -0,0 +1,9 @@
+from ex3 import Vehicule
+
+voiture= Vehicule("voiture",5)
+
+print(voiture.get_type()) #recupere et renvoie le nom 
+print(voiture.get_nbplace()) #recupere et renvoie le nom 
+
+print(voiture.set_type("moto")) #modifie le nom
+print(voiture.set_nbplace(2)) #modifie le nom
